57b2f087650ed807caa0838e5fa90bb2bc5a3ec5
Jackey <jaclun2003@skola.goteborg.se>
2020-10-20 15:50:06 +0200

    Added Thirteenth Lesson

---
+++ Programming Fundamentals/13 - Physics/Physics/Assets/Scripts/GoalChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalChecker: MonoBehaviour {
+	private Rigidbody2D rb2D;
+	private void Start() {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (!other.gameObject.CompareTag("Hoop")) return;
+
+		Debug.Log("You scored");
+		transform.position = Vector3.zero;
+		rb2D.velocity = Vector2.zero;
+	}
+}
---
+++ Programming Fundamentals/13 - Physics/Physics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private float speed = 5;
+	[SerializeField] private float jumpPower = 10;
+
+	private Rigidbody2D rb2D;
+	private SpriteRenderer spriteRenderer;
+	private Vector2 movement;
+	private bool grounded;
+	private bool jump;
+
+	// Start is called before the first frame update
+	private void Start() {
+		rb2D = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	private void Update() {
+		//Movement
+		float x = Input.GetAxis("Horizontal");
+
+		movement = new Vector2(x * speed, rb2D.velocity.y);
+
+		if (grounded && Input.GetButtonDown("Jump")) {
+			jump = true;
+		}
+	}
+
+	//Update is called every physics interval (0.02s) before physics
+
+	private void FixedUpdate() {
+		rb2D.velocity = movement;
+
+		if (jump) {
+			// Jump with impulse
+			// rb2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
+			// Jump with velocity
+			rb2D.velocity = new Vector2(movement.x, jumpPower);
+
+			spriteRenderer.flipY = !spriteRenderer.flipY;
+			jump = false;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Ground")) {
+			grounded = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Ground")) {
+			grounded = false;
+		}
+	}
+}
