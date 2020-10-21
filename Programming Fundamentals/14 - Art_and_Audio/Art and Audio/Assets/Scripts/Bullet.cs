85c9dd7945e79daa4a5c47f5cc3889cec1d97c3f
Jackey <jaclun2003@skola.goteborg.se>
2020-10-21 15:00:37 +0200

    Added Fourteenth Lesson

---
+++ Programming Fundamentals/14 - Art_and_Audio/Art and Audio/Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public AudioClip explosionSound;
+
+	private void Start() {
+		GetComponent<AudioSource>().pitch = Random.Range(0.5f, 1.5f);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+		Destroy(gameObject);
+	}
+}
