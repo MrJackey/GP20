f02e7ae69c96aedfd1a0abc2cd89ab5cc29fb197
Jackey <jaclun2003@skola.goteborg.se>
2020-11-03 15:20:57 +0100

    Added Sixteenth Lesson

---
+++ Programming Fundamentals/16 - State_Machines_and_Animation/State Machines and Animation/Assets/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+	private Animator anim;
+	private static readonly int OnHoverHash = Animator.StringToHash("onHover");
+	private static readonly int ExitHoverHash = Animator.StringToHash("exitHover");
+
+	private void Start() {
+		anim = GetComponent<Animator>();
+	}
+
+	public void Spin() {
+		anim.SetTrigger(OnHoverHash);
+	}
+
+	public void StopSpin() {
+		anim.SetTrigger(ExitHoverHash);
+	}
+}
